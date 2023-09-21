a91a88966f9b8411bccff326a4287bf16993f6d5
Nguyễn Hữu Mạnh <92002272+Silverss03@users.noreply.github.com>
2023-09-21 10:54:07 +0700

    Add files via upload

--- cards/templatetags/cards_tags .py
+++ cards/templatetags/cards_tags .py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from django import template
 
 from cards.models import BOXES, Card
@@ -14,4 +15,22 @@ def boxes_as_links():
             "card_count": card_count,
         })
 
+=======
+from django import template
+
+from cards.models import BOXES, Card
+
+register = template.Library()
+
+@register.inclusion_tag("cards/box_links.html")
+def boxes_as_links():
+    boxes = []
+    for box_num in BOXES:
+        card_count = Card.objects.filter(box=box_num).count()
+        boxes.append({
+            "number": box_num,
+            "card_count": card_count,
+        })
+
+>>>>>>> ea6e64420570d12c6036e759196c37fe4b20cb06
     return {"boxes": boxes}
--- flashcards/asgi.py
+++ flashcards/asgi.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 """
 ASGI config for flashcards project.
 
@@ -14,3 +15,21 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flashcards.settings')
 
 application = get_asgi_application()
+=======
+"""
+ASGI config for flashcards project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flashcards.settings')
+
+application = get_asgi_application()
+>>>>>>> ea6e64420570d12c6036e759196c37fe4b20cb06
