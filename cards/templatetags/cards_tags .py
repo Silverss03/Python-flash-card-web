a1a4345002e80a0307984233e1c5c28ca1ee23e4
Nguyễn Hữu Mạnh <92002272+Silverss03@users.noreply.github.com>
2023-09-21 11:37:28 +0700

    Add files via upload

--- cards/templatetags/cards_tags .py
+++ cards/templatetags/cards_tags .py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django import template
 
 from cards.models import BOXES, Card
@@ -15,22 +14,4 @@ def boxes_as_links():
             "card_count": card_count,
         })
 
-=======
-from django import template
-
-from cards.models import BOXES, Card
-
-register = template.Library()
-
-@register.inclusion_tag("cards/box_links.html")
-def boxes_as_links():
-    boxes = []
-    for box_num in BOXES:
-        card_count = Card.objects.filter(box=box_num).count()
-        boxes.append({
-            "number": box_num,
-            "card_count": card_count,
-        })
-
->>>>>>> ea6e64420570d12c6036e759196c37fe4b20cb06
     return {"boxes": boxes}
